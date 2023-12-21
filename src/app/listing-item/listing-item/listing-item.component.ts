dcedcfce39ae73f57ac84293e875bf42cfdca50a
Aymen Nacer <aymen@nacer.com>
2023-12-21 12:01:12 +0300

    Add recent property listing at home page

---
+++ src/app/listing-item/listing-item/listing-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-item',
+  templateUrl: './listing-item.component.html',
+  styleUrl: './listing-item.component.css'
+})
+export class ListingItemComponent {
+
+}
