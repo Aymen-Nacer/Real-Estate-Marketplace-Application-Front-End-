36282891f317ba15a20dce9ba7d9237579cb4a81
Aymen Nacer <aymen@nacer.com>
2023-12-26 11:58:21 +0300

    Add users array in Auth service and add sign up and login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root',
@@ -6,18 +7,47 @@ import { Injectable } from '@angular/core';
 export class AuthService {
   private isLoggedIn = false;
 
-  private currentUser = {
+  private users: User[] = [
+    {
+      userId: 2,
+      username: 'aymennacer',
+      password: '123', // Make sure to use string for the password
+    },
+    // Add more users as needed
+  ];
+
+  private currentUser: User = {
     userId: 0,
     username: '',
+    password: '', // Add a password property to the currentUser
   };
 
+  signup(username: string, password: string): boolean {
+    if (this.users.some((u) => u.username === username)) {
+      return false;
+    }
+
+    const newUser: User = {
+      userId: this.users.length + 1,
+      username,
+      password,
+    };
+
+    this.users.push(newUser);
+    this.isLoggedIn = true;
+    this.currentUser = newUser;
+
+    return true;
+  }
+
   login(username: string, password: string): boolean {
-    if (username === 'example' && password === 'password') {
+    const user = this.users.find(
+      (u) => u.username === username && u.password === password
+    );
+
+    if (user) {
       this.isLoggedIn = true;
-      this.currentUser = {
-        userId: 1,
-        username: username,
-      };
+      this.currentUser = user;
       return true;
     } else {
       this.isLoggedIn = false;
@@ -30,6 +60,7 @@ export class AuthService {
     this.currentUser = {
       userId: 0,
       username: '',
+      password: '',
     };
   }
 
@@ -37,7 +68,7 @@ export class AuthService {
     return this.isLoggedIn;
   }
 
-  getCurrentUser(): any {
+  getCurrentUser(): User {
     return this.currentUser;
   }
 }
