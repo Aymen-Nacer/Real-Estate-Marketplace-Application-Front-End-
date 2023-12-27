dff843dcc63145a9aa0a8ee29fbdca4ba927182d
Aymen Nacer <aymen@nacer.com>
2023-12-27 14:26:25 +0300

    Add profile page and user information and modify routes

---
+++ src/app/profile/profile/profile.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { NgForm } from '@angular/forms';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
+})
+export class ProfileComponent {
+  username = '';
+  password = '';
+
+  constructor(
+    public authService: AuthService,
+    private usersService: UsersService
+  ) {
+    this.authService.printProperties();
+  }
+
+  onProfileUpdate(profileForm: NgForm): void {
+    if (profileForm.valid) {
+      if (
+        this.usersService.findUserByCredentials(
+          this.authService.getCurrentUser().username,
+          this.password
+        )
+      ) {
+        const updatedUser: User = {
+          ...this.authService.getCurrentUser(),
+          username: this.username,
+        };
+        if (this.usersService.updateUser(updatedUser)) {
+          console.log(`the user ${this.username} was updated successfuly`);
+          this.authService.login(this.username, this.password);
+        } else {
+          console.log(`the user ${this.username} was not found for updating!`);
+        }
+      } else {
+        console.log(
+          `the user ${this.username} credentials are wrong try again!`
+        );
+      }
+      this.usersService.printProperties();
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+}
