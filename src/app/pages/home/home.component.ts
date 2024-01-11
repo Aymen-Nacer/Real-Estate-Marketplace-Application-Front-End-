04aa2b511ffd643b0cafa2d7e9bd199697af6f97
Aymen Nacer <aymen@nacer.com>
2024-01-11 20:37:19 +0300

    Improve the UI of the home page and changed project directory structure

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../../models/listing';
+import { ListingsService } from '../../services/listings.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css',
+})
+export class HomeComponent implements OnInit {
+  searchTerm = '';
+  listings: Listing[] = [];
+  private listingsSubscription: Subscription | undefined;
+
+  constructor(
+    private listingService: ListingsService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.listingsSubscription = this.listingService.listings$.subscribe(
+      (listings) => {
+        this.listings = listings;
+      }
+    );
+  }
+
+  onSubmitForm() {
+    console.log('search term is', this.searchTerm);
+    // this.router.navigate(['/search'], {
+    //  queryParams: { searchTerm: this.searchTerm, sortBy: 'Latest' },
+    //});
+  }
+
+  ngOnDestroy() {
+    if (this.listingsSubscription) {
+      this.listingsSubscription.unsubscribe();
+    }
+  }
+}
