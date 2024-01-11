db36f9c8066a08a1c022dfc48de0cdae323cc092
Aymen Nacer <aymen@nacer.com>
2024-01-11 20:36:57 +0300

    changed project directory structure

---
+++ src/app/pages/add-property/add-property/add-property.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ListingsService } from '../../services/listings.service';
+import { AuthService } from '../../services/auth.service';
+import { Listing } from '../../models/listing';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrl: './add-property.component.css',
+})
+export class AddPropertyComponent {
+  propertyName = '';
+  propertyDescription = '';
+  address = '';
+  bedrooms = -1;
+  bathrooms = -1;
+  parking = false;
+  furnished = false;
+  propertyPrice = -1;
+
+  constructor(
+    private router: Router,
+    private listingsService: ListingsService,
+    private authService: AuthService
+  ) {}
+
+  onAddProperty(newPropertyForm: NgForm): void {
+    if (newPropertyForm.valid) {
+      const listing: Listing = {
+        propertyId: 2,
+        imageUrls: [
+          'https://picsum.photos/800/300?random=1',
+          'https://picsum.photos/800/300?random=1',
+        ],
+        address: '123 Main St',
+        bedrooms: 3,
+        bathrooms: 2,
+        price: 100000,
+        parking: true,
+        furnished: false,
+        name: this.propertyName,
+        description: this.propertyDescription,
+        user: this.authService.getCurrentUser(),
+      };
+      this.listingsService.addProperty(listing);
+
+      console.log('added successfuly!');
+      console.log('listings', this.listingsService.printProperties());
+      this.router.navigate(['']);
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+}
