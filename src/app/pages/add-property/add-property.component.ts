881f0f1fb1759d44425b8b2f6fca2484a847f884
Aymen Nacer <aymen@nacer.com>
2024-01-29 12:13:31 +0300

    Add a new feature to upload images

--- src/app/pages/add-property/add-property.component.ts
+++ src/app/pages/add-property/add-property.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ListingsService } from '../../services/listings.service';
 import { Listing } from '../../models/listing';
 import { MessageService } from '../../services/message.service';
 import { AuthService } from '../../services/auth.service';
+import { ImageUploadService } from '../../services/image-upload.service';
+import { FileUpload } from '../../models/fileUpload';
 
 @Component({
   selector: 'app-add-property',
@@ -20,23 +22,80 @@ export class AddPropertyComponent {
   parking = false;
   furnished = false;
   propertyPrice = 0;
+  selectedFiles: FileList | null = null;
+  currentFileUpload?: FileUpload;
+  percentage = 0;
+  uploading = false;
+  currentUploadedFiles: FileUpload[] = [];
 
   constructor(
     private router: Router,
     private listingsService: ListingsService,
     private messageService: MessageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private imageUploadService: ImageUploadService
   ) {
     window.scrollTo(0, 0);
+    this.imageUploadService.uploadedFiles$.subscribe((uploadedFiles) => {
+      this.currentUploadedFiles = uploadedFiles;
+    });
+  }
+
+  selectFile(event: any): void {
+    if (
+      event.target.files.length > 0 &&
+      event.target.files.length + this.currentUploadedFiles.length < 7
+    ) {
+      this.selectedFiles = event.target.files;
+    } else {
+      event.target.value = null;
+      this.selectedFiles = null;
+      this.messageService.showAlert(
+        'You can only upload 6 images per listing',
+        'error'
+      );
+    }
+  }
+
+  onUpload(): void {
+    this.uploading = true;
+    if (this.selectedFiles) {
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        const file: File = this.selectedFiles[i];
+
+        this.currentFileUpload = new FileUpload(file);
+
+        this.imageUploadService
+          .pushFileToStorage(this.currentFileUpload)
+          .subscribe({
+            next: (percentage) => {
+              this.percentage = Math.round(percentage ? percentage : 0);
+            },
+            error: (error) => {
+              this.messageService.showAlert(
+                'Image upload failed (2 mb max per image)',
+                'error'
+              );
+              this.uploading = false;
+            },
+            complete: () => {
+              this.uploading = false;
+              this.percentage = 0;
+            },
+          });
+      }
+      this.selectedFiles = null;
+    }
+  }
+
+  handleRemoveImage(index: number) {
+    this.imageUploadService.deleteFileAtIndex(index);
   }
 
   onAddProperty(newPropertyForm: NgForm): void {
     if (newPropertyForm.valid) {
       const listing: Listing = {
-        imageUrls: [
-          'https://picsum.photos/800/300?random=1',
-          'https://picsum.photos/800/300?random=1',
-        ],
+        imageUrls: [],
         address: this.address,
         bedrooms: this.bedrooms,
         bathrooms: this.bathrooms,
@@ -47,6 +106,12 @@ export class AddPropertyComponent {
         description: this.propertyDescription,
         userId: this.authService.getCurrentUser().id.toString(),
       };
+
+      listing.imageUrls = this.currentUploadedFiles.map((file) => file.url);
+
+      if (listing.imageUrls.length === 0) {
+        listing.imageUrls.push('https://i.imgur.com/n6B1Fuw.jpg');
+      }
 
       console.log('listing to be added is', listing);
 
@@ -71,5 +136,13 @@ export class AddPropertyComponent {
         'error'
       );
     }
+  }
+
+  isExceedingLimit(): boolean {
+    const selectedFilesCount = this.selectedFiles
+      ? this.selectedFiles.length
+      : 0;
+    const totalFiles = selectedFilesCount + this.currentUploadedFiles.length;
+    return totalFiles > 6 || selectedFilesCount === 0;
   }
 }
